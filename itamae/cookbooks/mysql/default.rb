cccda530fd3342f591acbd3b31df678f3a1067a1
Sangyong Sim <sangyong-sim@cookpad.com>
2018-09-01 12:01:05 +0900

    Add itamae

---
+++ itamae/cookbooks/mysql/default.rb
@@ -0,0 +1,16 @@
+service 'mysql'
+
+%w(
+  /etc/mysql/mysql.cnf
+  /etc/mysql/conf.d/mysql.cnf
+  /etc/mysql/conf.d/mysqldump.cnf
+  /etc/mysql/mysql.conf.d/mysqld.cnf
+  /etc/mysql/mysql.conf.d/mysqld_safe_syslog.cnf
+).each do |file|
+  remote_file file do
+    owner 'root'
+    group 'root'
+    mode '644'
+    notifies :restart, 'service[mysql]'
+  end
+end
